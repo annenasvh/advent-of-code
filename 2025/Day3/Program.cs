1ffc9d4271d5a9a74f0762becbbfd6d9af7e85d6
annenas <annevanhattem@gmail.com>
2025-12-03 23:25:38 +0100

    2025 Day3 Part 2

--- 2025/Day3/Program.cs
+++ 2025/Day3/Program.cs
@@ -12,7 +12,7 @@ namespace Day3
 
             string[] banks = File.ReadAllLines(fileName);
 
-            int totalJoltage = 0;
+            long totalJoltage = 0;
             for (int i = 0; i < banks.Length; i++)
             {   
                 // convert string[] to int[]
@@ -22,20 +22,21 @@ namespace Day3
                     // banks[i][j] is char so casting to int gives ASCII value, solved by doing "-'0'"
                     bank[j] = banks[i][j] -'0';
                 }
-                totalJoltage += FindLargestJoltage(bank);
+                // totalJoltage += FindLargestJoltage1(bank);
+                totalJoltage += FindLargestJoltage2(bank);
             }
-
             Console.WriteLine($"Maxiumum joltage possible: {totalJoltage}");
-
+            
         }
 
-        static int FindLargestJoltage(int[] bank)
+        // Part 1
+        static long FindLargestJoltage1(int[] bank)
         {   
             // find the largest battery, excluding the last one, as we always pick two batteries
             (int firstDigit, int firstDigitIndex) = FindLargestDigit(bank[0..(bank.Length-1)]);
             (int secondDigit, _) = FindLargestDigit(bank[(firstDigitIndex+1)..bank.Length]);
 
-            return Int32.Parse(firstDigit.ToString() + secondDigit.ToString());
+            return Int64.Parse(firstDigit.ToString() + secondDigit.ToString());
         }
 
         // Given an int[] returns the value and index of the largest digit (e.g. [1,5,2,3] => (5,1)) 
@@ -53,6 +54,30 @@ namespace Day3
                 }
             }
             return (largest, largestIndex);
+        }
+
+        // Part 2
+        static long FindLargestJoltage2(int[] bank)
+        {   
+            string acc = "";
+
+            // first digit cannot be one of the last 11 digits
+            int necessaryTailLength = 11;
+
+            // necessary to correctly track the indices
+            int largestIndex = 0;
+
+            // Need to find 12 digits
+            for(int i = 0; i < 12; i++)
+            {
+                (int largest, int relativeLargestIndex) = FindLargestDigit(bank[largestIndex..(bank.Length-necessaryTailLength)]);
+                largestIndex += relativeLargestIndex + 1;
+
+                acc += largest.ToString();
+                necessaryTailLength -= 1;
+            }
+
+            return Int64.Parse(acc);
         }
     }
 }

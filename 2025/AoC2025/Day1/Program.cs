8176b186e653cf5f6591005235b7c7d67cc86cca
annenas <annevanhattem@gmail.com>
2025-12-03 18:11:59 +0100

    Complete Day 1 Part 2

--- 2025/AoC2025/Day1/Program.cs
+++ 2025/AoC2025/Day1/Program.cs
@@ -22,12 +22,14 @@ namespace Day1
                 bool TurnDirectionRight = Moves[i][0] == 'R';
                 int TurnAmount = int.Parse(Moves[i].Substring(1));
 
-                DialLocation = TurnDial(DialLocation, TurnDirectionRight, TurnAmount);
-
-                // Console.WriteLine($"Processing Move: {Moves[i]}.");
+                // Console.WriteLine($"Zeros counted: {DoorPassword}.\n ------------");
+                // Console.WriteLine($"Move: {Moves[i]}.");
                 // Console.WriteLine($"Dial Location: {DialLocation}.");
+                (DialLocation, int ExtraZeros) = TurnDial(DialLocation, TurnDirectionRight, TurnAmount);
 
-                // Track door password (number of times dial lands on 0)
+
+                // Track door password (number of times dial lands on 0 + number of times it passes 0)
+                DoorPassword += ExtraZeros;
                 if (DialLocation == 0) {
                     DoorPassword += 1;
                 }
@@ -37,27 +39,53 @@ namespace Day1
             Console.WriteLine($"Door password: {DoorPassword} (Dial Location: {DialLocation})");
         }
 
-        static int TurnDial(int DialLocation, bool TurnRight, int TurnAmount)
+        static (int Location, int ZeroCount) TurnDial(int DialLocation, bool TurnRight, int TurnAmount)
         {
             int NewDialLocation;
+
+            int ZeroCounter = 0;
+
+
             if (TurnRight)
             {
                 NewDialLocation = DialLocation + TurnAmount;
                 while (NewDialLocation > 99)
                 {
+                    // Prevent double counting if we land on zero exactly
+                    if (NewDialLocation == 100)
+                    {
+                        ZeroCounter -= 1;
+                    }
+                    // Console.WriteLine($"\tLocation computed: {NewDialLocation}");
                     NewDialLocation -= 100;
+                    ZeroCounter += 1;                  
                 }
+            
             }
             else
             {
                 NewDialLocation = DialLocation - TurnAmount;
                 while (NewDialLocation < 0 )
                 {
+                    // Console.WriteLine($"\tLocation computed: {NewDialLocation}");
                     NewDialLocation += 100;
+                    ZeroCounter += 1;
+                }
+
+                // If we start at 0, we do not want to count that as passing zero (e.g. L5 from 0 moves to -5)
+                if (DialLocation == 0)
+                {
+                    ZeroCounter -= 1;
+                }
+
+                // This should never happen
+                if (ZeroCounter < 0)
+                {
+                    Console.WriteLine("Error: Negative zero counter.");
                 }
             }
 
-            return NewDialLocation;
+            return (NewDialLocation, ZeroCounter);
         }
     }
 }
